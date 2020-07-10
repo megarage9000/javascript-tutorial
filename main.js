7b16ac46e5625ba732d6e7f7ed7455e1db5b13b0
megarage9000 <gtom_1984@hotmail.ca>
2020-07-10 14:18:24 -0700

    Nullish coalescing operator ??
    - Link: https://javascript.info/logical-operators

--- main.js
+++ main.js
@@ -10,46 +10,18 @@
 // "use strict" enables us to use old functionality
 "use strict"
 
-// Logical Operators
+// Nullish coalescing operator '??'
+
 /*
+    Operates similarly to || (OR), and && (AND), excepts
+    it finds the first non undefined/null value
+        - If there isn't any, return the last value
 
-    - Similar to other programs except:
+    -  Good for assigning defaults when necessary
+    -  It shall not be used with || or && for safety reasons
+ */
 
-        1. '||' (OR) finds the first truth value out of the operands
-            - ex. result = value1 || value2 || value3, will return the 
-            first value that is true from left to right to result
-                - If none are true, return the last value
-
-        2. '&&' (AND) finds the first false value out of the operands
-            - ex. result = value1 && value2 && value3, will return the 
-            first false value from left to right to 
-                - If none are false, return last value
-
-        3. !! Is a shorthand for Boolean(value)
- 
-*/
-
-// Check login challenge
-
-const adminName = "Admin";
-const password = "123";
-let userName = prompt("Who's there? ");
-
-if(userName == "" || userName == null){
-    alert("Cancelled.");
-}
-else if(userName !== adminName){
-    alert("I don't know you.");
-}
-else{
-    let userGuess = prompt("Password?");
-    if(userGuess == "" || userGuess == null){
-        alert("Cancelled");
-    }
-    else if(userGuess !== password){
-        alert("Wrong password.");
-    }
-    else{
-        alert("Welcome!");
-    }
-}
+ let value = null;
+ let value2 = undefined;
+ let value3 = prompt("Enter a non null or undefined value: ");
+ alert(value ?? value2 ?? value3) // will alert value3 if not undefined or null
