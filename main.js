87544eb2be2ddcec3c9b84eef30c0ff98275320b
megarage9000 <gtom_1984@hotmail.ca>
2020-07-09 20:23:48 -0700

    Data Types
    - Link: https://javascript.info/types

--- main.js
+++ main.js
@@ -10,13 +10,45 @@
 // "use strict" enables us to use old functionality
 "use strict"
 
-alert("Hello World again!");
+// alert("Hello World again!");
 
-// Variable differences:
+// Datatypes:
 
-// "let" is simple declaration of a variable, which can change over time
-let user = "John";
-user = "Josh";
+// - Number (Similar to integer)
+let regularInt = 23;
+alert("Regular int" + regularInt);
 
-// "const" is also another declaration, but for non changing variables
-const PI = 3.14;
+// - BigInt (Similar to floats)
+const largeInt = 21312421412n;
+alert("Big int" + largeInt);
+// - Strings
+let singleQuoteString = 'Hello world';
+let doubleQuoteString = "It's me again";
+
+// This type of string allows us to embed variables or expressions in the braces
+let backQuoteString = `${singleQuoteString}. ${doubleQuoteString}, and I am learning javascript today!`;
+alert(backQuoteString);
+
+// - Boolean
+let randomBooleanExpresion = 2 < 5;
+let randomBoolean = true && randomBooleanExpresion;
+alert("Boolean expression example: " + randomBoolean);
+
+// - Undefined
+// not recommended for use, but its there
+let anUndefinedVar;
+alert("Undefined value: " + anUndefinedVar);
+
+// - Null
+let nullValue = null;
+alert("Null value: " + nullValue);
+
+// - New Operator: typeof
+// allows us to get the type of the object, in a string
+// - note null returns object lol
+
+function randomFunction(){
+    alert("I will not run");
+}
+
+alert(typeof(randomFunction));
