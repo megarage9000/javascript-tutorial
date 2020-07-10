aae5af10ad596815a52acf0614fb04306851d420
megarage9000 <gtom_1984@hotmail.ca>
2020-07-10 14:09:29 -0700

    Logical Operators
    - Link: https://javascript.info/logical-operators

--- main.js
+++ main.js
@@ -10,17 +10,46 @@
 // "use strict" enables us to use old functionality
 "use strict"
 
-// Ifs and ternary operators
-// - Similar to other languages
-
+// Logical Operators
 /*
-    - Ternary review:
 
-    (condition) ? value1 : value2
+    - Similar to other programs except:
 
-        - if condition is true:
-            value1 is returned/exectuted
-        - else:
-            value2 is returned/executed
+        1. '||' (OR) finds the first truth value out of the operands
+            - ex. result = value1 || value2 || value3, will return the 
+            first value that is true from left to right to result
+                - If none are true, return the last value
+
+        2. '&&' (AND) finds the first false value out of the operands
+            - ex. result = value1 && value2 && value3, will return the 
+            first false value from left to right to 
+                - If none are false, return last value
+
+        3. !! Is a shorthand for Boolean(value)
+ 
 */
 
+// Check login challenge
+
+const adminName = "Admin";
+const password = "123";
+let userName = prompt("Who's there? ");
+
+if(userName == "" || userName == null){
+    alert("Cancelled.");
+}
+else if(userName !== adminName){
+    alert("I don't know you.");
+}
+else{
+    let userGuess = prompt("Password?");
+    if(userGuess == "" || userGuess == null){
+        alert("Cancelled");
+    }
+    else if(userGuess !== password){
+        alert("Wrong password.");
+    }
+    else{
+        alert("Welcome!");
+    }
+}
