3279618f0b6709d3698fde68fdf322b9f8540a19
megarage9000 <gtom_1984@hotmail.ca>
2020-07-09 19:17:10 -0700

    Code Structure - Variables
    - Code Structure: https://javascript.info/structure
    - Use Strict: https://javascript.info/strict-mode
    - Variables: https://javascript.info/variables

--- main.js
+++ main.js
@@ -1,3 +1,22 @@
 // This is important
 /* Manual and Specifications: https://javascript.info/manuals-specifications*/
+
+// This is a single line comment.
+/*
+    This is a multi line comment.
+    you can enter mutliple lines
+*/
+
+// "use strict" enables us to use old functionality
+"use strict"
+
 alert("Hello World again!");
+
+// Variable differences:
+
+// "let" is simple declaration of a variable, which can change over time
+let user = "John";
+user = "Josh";
+
+// "const" is also another declaration, but for non changing variables
+const PI = 3.14;
