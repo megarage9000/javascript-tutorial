d8b5dd91e19544733ce2b774c82d88494a2bf8ca
megarage9000 <gtom_1984@hotmail.ca>
2020-07-11 15:19:19 -0700

    Methods of Primitives
    - Link: https://javascript.info/primitives-methods

--- main.js
+++ main.js
@@ -1,2 +1,22 @@
 // This is important
 /* Manual and Specifications: https://javascript.info/manuals-specifications*/
+
+
+"use strict"
+
+// Methods of primitives
+
+/*
+    Summary from website: 
+
+    
+    - Primitives except null and undefined provide many helpful methods. We will study those in the upcoming chapters.
+
+    - Formally, these methods work via temporary objects, but JavaScript engines are well tuned to optimize that 
+    internally, so they are not expensive to call.
+
+*/
+
+const NAME = "John";
+let upperCaseName = NAME.toUpperCase();
+alert(upperCaseName);
