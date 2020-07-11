6557d946e7e2d8f1422ef5b18e6bf050f5d34b1f
megarage9000 <gtom_1984@hotmail.ca>
2020-07-11 16:32:52 -0700

    Strings
    - Link: https://javascript.info/string

--- main.js
+++ main.js
@@ -9,75 +9,57 @@
 /*
     Summary from website: 
 
+    - There are 3 types of quotes. Backticks allow a string to span multiple lines and embed expressions ${…}.
     
-    To write numbers with many zeroes:
-
-        - Append "e" with the zeroes count to the number. 
-        Like: 123e6 is the same as 123 with 6 zeroes 123000000.
+    - Strings in JavaScript are encoded using UTF-16.
     
-        - A negative number after "e" causes the number to be 
-        divided by 1 with given zeroes. E.g. 123e-6 means 0.000123 (123 millionths).
+    - We can use special characters like \n and insert letters by their unicode using \u....
+    
+    - To get a character, use: [].
+    
+    - To get a substring, use: slice or substring.
+    
+    - To lowercase/uppercase a string, use: toLowerCase/toUpperCase.
+    
+    - To look for a substring, use: indexOf, or includes/startsWith/endsWith for simple checks.
+    
+    - To compare strings according to the language, use: localeCompare, otherwise they are compared by character codes.
 
-    For different numeral systems:
+    There are several other helpful methods in strings:
 
-        - Can write numbers directly in hex (0x), octal (0o) and binary (0b) systems.
-        
-        - parseInt(str, base) parses the string str into an integer in numeral system 
-        with given base, 2 ≤ base ≤ 36.
-        
-        - num.toString(base) converts a number to a string in the numeral system with 
-        the given base.
+        str.trim() – removes (“trims”) spaces from the beginning and end of the string.
+        str.repeat(n) – repeats the string n times.
+        …and more to be found in the manual.
 
-    For converting values like 12pt and 100px to a number:
+    Strings also have methods for doing search/replace with regular expressions. But that’s big topic, 
+    so it’s explained in a separate tutorial section Regular expressions.
 
-        - Use parseInt/parseFloat for the “soft” conversion, which reads a number from a 
-        string and then returns the value they could read before the error.
-
-    For fractions:
-
-        - Round using Math.floor, Math.ceil, Math.trunc, Math.round or num.toFixed(precision).
-        
-        - Make sure to remember there’s a loss of precision when working with fractions.
-
-    More mathematical functions:
-
-        - See the Math object when you need them. The library is very small, but can cover basic needs.
-
-
-    Additonals:
-        
-        - To call Number methods on an actual number, use double dots:
-            - ex. 12345..toString()
-
-        - You can use Object.is(value1, value2) that does === more reliably
-            - handles NaN === NaN and +0 === -0
+    - IN SUMMARY... view the manual.
 */
 
-// Sum Visitors challenge
-
-// function sum(num1, num2){
-//     return num1 + num2;
+// - Uppercase First Letter
+// function ucFirst(word){
+//     if(!word) return word;
+//     let result = 
+//         word[0].toUpperCase()
+//          + word.slice(0, word.length - 1);
+//     return result;
 // }
-// let num1 = parseInt(prompt("Enter value 1: ")); // you can also do + operator
-// let num2 = parseInt(prompt("Enter value 2: "));
-// alert(sum(num1, num2));
 
-// Why 6.35..toFixed(1) -> 6.3? How to fix?
+// let word = prompt("Enter a word");
+// alert(ucFirst(word));
 
-// let num = Math.round(6.35 * 10) / 10;
-// alert("Original Num: " + 6.35.toFixed(1));
-// alert("With rounding: " + num);
+// - Check for Spam
+// - use the newer .includes()
+// function checkSpam(sourceString){
 
-// randomInteger challenge
-// - Allow interval expansion for:
-//      - min = min - 0.5
-//      - max = max + 0.5
-//      and round afterwards
-function randomInteger(min, max){
-    let newMin = min - 0.5;
-    let newMax = max + 0.5;
-    let result = newMin + Math.random() * (newMax - newMin);
-    return Math.round(result);
-}
+//     let newSource = sourceString.toLowerCase();
+//     let isSpam = newSource.includes('xxx') || newSource.includes('viagra');
 
-alert(randomInteger(1, 5));
+//     return isSpam;
+// }
+
+// alert(checkSpam('buy ViAgRA now') == true);
+// alert(checkSpam('free xxxxx') == true);
+// alert(checkSpam('Cool guys don\'t have sex') == false);
+
