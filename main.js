7cb6a33819a435e9380722403e6787fc18a1cb3a
megarage9000 <gtom_1984@hotmail.ca>
2020-07-12 00:54:58 -0700

    Function Object -> New Function()
    - Function Object(): https://javascript.info/function-object
    - New Function(): https://javascript.info/new-function

--- main.js
+++ main.js
@@ -1,42 +1,80 @@
 // This is important
 /* Manual and Specifications: https://javascript.info/manuals-specifications*/
 
-// The old "var"
+// Function Objects, NFE -> New Function()
 /*
     Summary on website
-    
-    [The old "var"]: 
+    [Function Objects, NFE]:
 
-    There are two main differences of var compared to let/const:
+        Functions are objects.
 
-        1. var variables have no block scope, they are visible minimum at the function level.
-    
-        2. var declarations are processed at function start (script start for globals).
+        Here we covered their properties:
 
-    There’s one more very minor difference related to the global object, that we’ll cover in the next chapter.
+            1. name – the function name. Usually taken from the function definition, 
+            but if there’s none, JavaScript tries to guess it from the context (e.g. an assignment).
 
-    These differences make var worse than let most of the time. Block-level variables is such a great thing. 
-    That’s why let was introduced in the standard long ago, and is now a major way (along with const) to declare a variable.
+            2. length – the number of arguments in the function definition. Rest parameters are not counted.
 
-    [Global Object]:
+        If the function is declared as a Function Expression (not in the main code flow), and it carries the name, 
+        then it is called a Named Function Expression. The name can be used inside to reference itself, for recursive calls or such.
+        
+        Also, functions may carry additional properties. Many well-known JavaScript libraries make great use of this feature.
+        They create a “main” function and attach many other “helper” functions to it. For instance, the
+        jQuery library creates a function named $. The lodash library creates a function _, and then adds _.clone,
+        _.keyBy and other properties to it (see the docs when you want learn more about them). Actually, they do it to 
+        lessen their pollution of the global space, so that a single library gives only one global variable. That reduces 
+        the possibility of naming conflicts.
 
-    The global object holds variables that should be available everywhere.
+        So, a function can do a useful job by itself and also carry a bunch of other functionality in properties.
 
-    That includes JavaScript built-ins, such as Array and environment-specific values, 
-    such as window.innerHeight – the window height in the browser.
+    [New Function]:
 
-    The global object has a universal name globalThis.
-    …But more often is referred by “old-school” environment-specific 
-    names, such as window (browser) and global (Node.js). As globalThis is a recent 
-    proposal, it’s not supported in non-Chromium Edge (but can be polyfilled).
+        The syntax:
 
-    We should store values in the global object only if they’re truly global for our project.
-    And keep their number at minimum.
+            let func = new Function ([arg1, arg2, ...argN], functionBody);
 
-    In-browser, unless we’re using modules, global functions and variables declared with var 
-    become a property of the global object.
+        For historical reasons, arguments can also be given as a comma-separated list.
 
-    To make our code future-proof and easier to understand, we should access properties of the 
-    global object directly, as window.x.
+        These three declarations mean the same:
+
+        new Function('a', 'b', 'return a + b'); // basic syntax
+        new Function('a,b', 'return a + b'); // comma-separated
+        new Function('a , b', 'return a + b'); // comma-separated with spaces
+
+        Functions created with new Function, have [[Environment]] referencing the global 
+        Lexical Environment, not the outer one. Hence, they cannot use outer variables. 
+        But that’s actually good, because it insures us from errors. Passing parameters 
+        explicitly is a much better method architecturally and causes no problems with minifiers.
 
 */
+
+// - Set and Decrease Counter 
+// Solution on website!
+function makeCounter() {
+    
+    let count = 0;
+
+    function counter(){
+        return count++;
+    }
+
+    counter.set = value => count = value;
+
+    counter.decrease = () => count--;
+
+    return counter;
+    // ... your code ...
+}
+  
+let counter = makeCounter();
+
+alert( counter() ); // 0
+alert( counter() ); // 1
+
+counter.set(10); // set the new count
+
+alert( counter() ); // 10
+
+counter.decrease(); // decrease the count by 1
+
+alert( counter() ); // 10 (instead of 11)
