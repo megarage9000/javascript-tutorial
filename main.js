866f91a9c16076c35ec654b15088be5140f18aeb
megarage9000 <gtom_1984@hotmail.ca>
2020-07-12 00:05:17 -0700

    Variable Scope
    - Link: https://javascript.info/closure

--- main.js
+++ main.js
@@ -1,43 +1,101 @@
 // This is important
 /* Manual and Specifications: https://javascript.info/manuals-specifications*/
 
-// Rest parameters
+// Variable Scopes
 /*
-    When we see "..." in the code, it is either rest parameters or the spread syntax.
-
-    There’s an easy way to distinguish between them:
-
-        1. When ... is at the end of function parameters, it’s 
-        “rest parameters” and gathers the rest of the list of arguments into an array.
+    Lexical Environments!  
+        - Each of these store the following: 
+            1. Environment Record [All local variables are stored as properties, and also 'this']
+            2. Reference to its outer Lexical Environment, and 
         
-        2. When ... occurs in a function call or alike, it’s 
-        called a “spread syntax” and expands an array into a list.
+        - A variable is essentially a 'property' of the Environment Record object
+            - The variable is uninitialized -> undefined when let declaration is seen 
+            -> then initialized
+            - When a variable is updated, it is updated in its lexical environment
+            - For garbage collection, when these variables aren't used, they may
+            not be available in the debugger!
 
-    Use patterns:
+        - Function Declarations are instantly initialized, unlike variables
+            - An inner lexical environment is created for local variables and 
+            arguments
+            - An outer lexical environment is referenced for outer variables
+            - On variable search, it searches the inner environment, then
+            to the outer environment
 
-        1. Rest parameters are used to create functions that accept 
-        any number of arguments.
-        
-        2. The spread syntax is used to pass an array to functions that 
-        normally require a list of many arguments.
 
-    Together they help to travel between a list and an array of parameters with ease.
-
-    All arguments of a function call are also available in “old-style” arguments: array-like iterable object.
 */
 
-function sum (...numbers){
-    let sum = 0;
-    for (number of numbers){
-        sum += number;
+// - Apprently you can do this?
+function sum(num1){
+    return function(num2){
+        return num1 + num2;
     }
-    return sum;
 }
 
-let randomNumbers = [1, 2, 3, 4, 5, 6];
-alert(sum(...randomNumbers));
+// alert(sum(1)(2));
 
-// you can also use .. to copy objects/arrays
-let arrayCopy = [...randomNumbers];
-alert(arrayCopy);
-alert(arrayCopy === randomNumbers);
+// - Filter through function
+/*
+    inBetween and inArray return Lexical environments
+    to their respective inner functions, allowing usage
+    for the fn() in filter for (item, index ...)
+*/
+function inBetween(min, max){
+    return function(item){
+        return item >= min && item <= max;
+    }
+}
+
+function inArray(arr){
+    return function(item){
+        return arr.includes(item);
+    }
+}
+
+let arr = [1, 2, 3, 4, 5, 6, 7];
+
+// alert( arr.filter(inBetween(3, 6)) ); // 3,4,5,6
+
+// alert( arr.filter(inArray([1, 2, 10])) ); // 1,2
+
+// - Sort by field
+function byField(fieldName){
+    return function(userA, userB){
+        return userA[fieldName] > userB[fieldName] ? 1 : -1;
+    }
+}
+
+let users = [
+    { name: "John", age: 20, surname: "Johnson" },
+    { name: "Pete", age: 18, surname: "Peterson" },
+    { name: "Ann", age: 19, surname: "Hathaway" }
+];
+
+let sortedByName = [...users.sort(byField('name'))];
+let sortedByAge = [...users.sort(byField('age'))];
+
+// - Army of Functions
+function makeArmy() {
+    let shooters = [];
+
+    let i = 0;
+    while (i < 10) {
+        // add a different variable to reference, which is created
+        // every new iteration of loop as a new lexical environment
+        // is created
+        let j = i;
+        let shooter = function() { // shooter function
+            alert( j ); // should show its number
+        };
+        shooters.push(shooter);
+        i++;
+    }
+
+    return shooters;
+}
+  
+let army = makeArmy();
+
+army[0](); // the shooter number 0 shows 10
+army[5](); // and number 5 also outputs 10...
+// ... all shooters show 10 instead of their 0, 1, 2, 3...
