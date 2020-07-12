dd00d8dd983f4b5e22c293e8c1825d37c63f6e8a
megarage9000 <gtom_1984@hotmail.ca>
2020-07-11 19:15:34 -0700

    Iterable
    - Link: https://javascript.info/iterable

--- main.js
+++ main.js
@@ -4,211 +4,55 @@
 
 "use strict"
 
-// Arrays
+// Iterable
 
 /*
     Summary from website: 
 
-    A cheat sheet of array methods:
+    Objects that can be used in for..of are called iterable.
 
-    1. To add/remove elements:
-        a. push(...items) – adds items to the end,
+    Technically, iterables must implement the method named Symbol.iterator.
+        The result of obj[Symbol.iterator] is called an iterator. It handles the further iteration process.
+        An iterator must have the method named next() that returns an object {done: Boolean, value: any}, 
+        here done:true denotes the end of the iteration process, otherwise the value is the next value.
 
-        b. pop() – extracts an item from the end,
+    The Symbol.iterator method is called automatically by for..of, but we also can do it directly.
+    Built-in iterables like strings or arrays, also implement Symbol.iterator.
+    String iterator knows about surrogate pairs.
 
-        c. shift() – extracts an item from the beginning,
+    Objects that have indexed properties and length are called array-like. Such objects may also have 
+    other properties and methods, but lack the built-in methods of arrays.
 
-        d. unshift(...items) – adds items to the beginning.
+    If we look inside the specification – we’ll see that most built-in methods assume that they work with 
+    iterables or array-likes instead of “real” arrays, because that’s more abstract.
 
-        e. splice(pos, deleteCount, ...items) – at index pos 
-        delete deleteCount elements and insert items.
+    Array.from(obj[, mapFn, thisArg]) makes a real Array of an iterable or array-like obj, and we can then 
+    use array methods on it. The optional arguments mapFn and thisArg allow us to apply a function to each item.
+*/
 
-        f. silce(start, end) – creates a new array, copies 
-        elements from position start till end (not inclusive) into it.
-
-        g. concat(...items) – returns a new array: copies all members 
-        of the current one and adds items to it. If any of items is an 
-        array, then its elements are taken.
-
-    2. To search among elements:
-        a. indexOf/lastIndexOf(item, pos) – look for item starting from 
-        position pos, return the index or -1 if not found.
-        
-        b. includes(value) – returns true if the array has value, 
-        otherwise false.
-        
-        c. find/filter(func) – filter elements through the function, return 
-        first/all values that make it return true.
-        
-        d. findIndex is like find, but returns the index instead of a value.
-
-    3. To iterate over elements:
-        a. forEach(func) – calls func for every element, does not return 
-        anything.
-
-    4. To transform the array:
-        a. map(func) – creates a new array from results of calling func for 
-        every element.
-        
-        b. sort(func) – sorts the array in-place, then returns it.
-        
-        c. reverse() – reverses the array in-place, then returns it.
-        
-        d. split/join – convert a string to array and back.
-        
-        e. reduce(func, initial) – calculate a single value over the array by 
-        calling func for each element and passing an intermediate result 
-        between the calls.
-
-    Additionally:
-        Array.isArray(arr) checks arr for being an array.
-
-    Please note that methods sort, reverse and splice modify the array itself.
-
-    These methods are the most used ones, they cover 99% of use cases. 
-    But there are few others:
-
-        arr.some(fn)/arr.every(fn) checks the array.
-
-        The function fn is called on each element of the array similar to map. 
-        If any/all results are true, returns true, otherwise false.
-
-        arr.fill(value, start, end) – fills the array with repeating value from index start to end.
-
-        arr.copyWithin(target, start, end) – copies its elements from position start 
-        till position end into itself, at position target (overwrites existing).
-
-    For the full list, see the manual. From the first sight it may seem that there are so many methods, 
-    quite difficult to remember. But actually that’s much easier. Look through the cheat sheet just to be 
-    aware of them. Then solve the tasks of this chapter to practice, so that you have experience with array methods.
-    Afterwards whenever you need to do something with an array, and you don’t know how – come here, look at 
-    the cheat sheet and find the right method. Examples will help you to write it correctly. Soon you’ll automatically
-    remember the methods, without specific efforts from your side
-
-*/  
-
-// - Camelize
-// - string slice only needs the beginning index, assumes empty is until last position
-// function camelize(stringSource){
-//     let strArray = stringSource.split("-");
-//     let result = strArray.map(
-//         (word, index) => index === 0 ? word : word[0].toUpperCase() + word.slice(1));
-//     return result.join("");
-        
-// }
-
-// alert(camelize("background-color") == 'backgroundColor');
-// alert(camelize("list-style-image") == 'listStyleImage');
-// alert(camelize("-webkit-transition") == 'WebkitTransition');
-
-// - Filter Range && Filter Range In Place
-// function filterRange(arr, min, max){
-//     return arr.filter(
-//         item => (item >= min && item <= max)
-//     );
-// }
-
-// function filterRangeInPlace(arr, min, max){
+// An example on the website:
+let range = {
+    from: 1,
+    to: 5,
     
-//     for(let i = 0; i < arr.length; i ++){
-//         let value = arr[i];
-//         if(value < min || value > max){
-//             arr.splice(i, 1);
-//             i--;
-//         }
-//     }
-// }
-// let arr = [5, 3, 8, 1];
-// let filtered = filterRange(arr, 1, 4);
-// alert( filtered ); // 3,1 (matching values)
-// alert( arr ); // 5,3,8,1 (not modified)
-// filterRangeInPlace(arr, 1, 4); // removed the numbers except from 1 to 4
-// alert( arr ); // [3, 1]
-
-// - Map to names
-
-// let john = { name: "John", age: 25 };
-// let pete = { name: "Pete", age: 30 };
-// let mary = { name: "Mary", age: 28 };
-
-// let users = [ john, pete, mary ];
-
-// let names = users.map(
-//     value => value.name
-// );
-
-// alert( names ); // John, Pete, Mary
-
-// - Map to Objects
-
-// let john = { name: "John", surname: "Smith", id: 1 };
-// let pete = { name: "Pete", surname: "Hunt", id: 2 };
-// let mary = { name: "Mary", surname: "Key", id: 3 };
-
-// let users = [ john, pete, mary ];
-
-
-// let usersMapped = users.map(
-//     user => ({ // wrap around additional () to treat as an object, not a function
-//         fullName: user.name + " " + user.surname,
-//         id: user.id,
-//     })
-// );
-
-// /*
-// usersMapped = [
-//   { fullName: "John Smith", id: 1 },
-//   { fullName: "Pete Hunt", id: 2 },
-//   { fullName: "Mary Key", id: 3 }
-// ]
-// */
-
-// alert( usersMapped[0].id ) // 1
-// alert( usersMapped[0].fullName ) // John Smith
-
-// // - Unique Array
-// function unique(arr) {
+    // Create a symbol iterator, and have it return itself, since
+    // contains the next() function
+    [Symbol.iterator]() {
+      this.current = this.from;
+      return this;
+    },
     
-//     return arr.filter( // checks the first occurence of the item, if it doesn't
-//                        // match the current index, don't take it in
-//                        // - from a comment!
-//         (item, index) => arr.indexOf(item) === index
-//     );
-    
-// }
+    // next relies on this.current, and is determined by the
+    // returning values of done and value
+    next() {
+      if (this.current <= this.to) {
+        return { done: false, value: this.current++ };
+      } else {
+        return { done: true };
+      }
+    }
+  };
   
-// let strings = ["Hare", "Krishna", "Hare", "Krishna",
-// "Krishna", "Krishna", "Hare", "Hare", ":-O"
-// ];
-
-// alert( unique(strings) ); // Hare, Krishna, :-O
-
-// - Keyed object from array
-// function groupById(array){
-//     let result = array.reduce(
-//         (object, value) =>
-//         { 
-//             // from website solution!
-//             object[value.id] = value
-//             return object
-//         }, {}
-//     )
-//     return result;
-// }
-// let users = [
-//     {id: 'john', name: "John Smith", age: 20},
-//     {id: 'ann', name: "Ann Smith", age: 24},
-//     {id: 'pete', name: "Pete Peterson", age: 31},
-//   ];
-  
-//   let usersById = groupById(users);
-//   alert(usersById);
-//   /*
-//   // after the call we should have:
-  
-//   usersById = {
-//     john: {id: 'john', name: "John Smith", age: 20},
-//     ann: {id: 'ann', name: "Ann Smith", age: 24},
-//     pete: {id: 'pete', name: "Pete Peterson", age: 31},
-//   }
-//   */
+  for (let num of range) {
+    alert(num); // 1, then 2, 3, 4, 5
+  }
