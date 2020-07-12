c4a820e7f95f1371bd19c550014f17ac3a5a5b8c
megarage9000 <gtom_1984@hotmail.ca>
2020-07-12 15:20:37 -0700

    Function Binding
    - Link: https://javascript.info/bind

--- main.js
+++ main.js
@@ -5,138 +5,107 @@
 /*
     Summary on website
     
-    - Decorator is a wrapper around a function that alters its behavior. The main 
-    job is still carried out by the function.
+    Method func.bind(context, ...args) returns a “bound variant” of function func that fixes the context this and first arguments if given.
 
-    - Decorators can be seen as “features” or “aspects” that can be added to a function. 
-    We can add one or add many. And all this without changing its code!
+    Usually we apply bind to fix this for an object method, so that we can pass it somewhere. For example, to setTimeout.
 
-    - To implement cachingDecorator, we studied methods:
+    When we fix some arguments of an existing function, the resulting (less universal) function is called partially applied or partial.
 
-        1. func.call(context, arg1, arg2…) – calls func with given context and arguments.
-        
-        2. func.apply(context, args) – calls func passing context as this and array-like
-        args into a list of arguments.
-
-    - The generic call forwarding is usually done with apply:
-
-        let wrapper = function() {
-        return original.apply(this, arguments);
-        };
-
-        We also saw an example of method borrowing when we take a method from an object and call 
-        it in the context of another object. It is quite common to take array methods and apply them 
-        to arguments. The alternative is to use rest parameters object that is a real array. 
-    
-    There are many decorators there in the wild. Check how well you got them by solving the tasks of this chapter.
-
+    Partials are convenient when we don’t want to repeat the same argument over and over again. Like if we have a send(from, to) function, and from should always be the same for our task, we can get a partial and go on with it.
 */
 
-// - Spy Decorator
-function work(a, b) {
-    alert( a + b ); // work is an arbitrary function or method
-}
-
-function spy(func){
-
-    // Create a wrapper that takes in variable
-    // arguments
-    function spyWrapper(...arguments){
-        spyWrapper.calls.push(arguments);
-        return func.apply(this, arguments);
-    };
-
-    // initialize calls to empty array
-    spyWrapper.calls = []
-    return spyWrapper;
-    
-}
-
-// work = spy(work);
-
-// (work(1, 2)); // 3
-// (work(4, 5)); // 9
-
-// for (let args of work.calls) {
-//     alert( 'call:' + args.join() ); // "call:1,2", "call:4,5"
+// What will this output?
+// - null, since it is set in user g: f.bind(null)
+// function f() {
+//     alert( this ); // ?
 // }
 
-// - Delaying Decorator
-function f(x) {
-    alert(x);
+// let user = {
+//     g: f.bind(null)
+// };
+  
+// user.g();
+
+
+// Second bind
+// - ouputs only John
+/*
+    bind is an exotic function that only remembers context
+    and arguments at creation time, thus it can't be 
+    rebinded.
+*/
+// function f() {
+//     alert(this.name);
+// }
+  
+// f = f.bind( {name: "John"} ).bind( {name: "Ann" } );
+
+// f();
+
+
+
+// Function property after bind
+// - outputs undefined
+/*
+    bound is a new function object of sayHi with binded
+    object with name : "John". Since the original
+    sayHi didn't include a .test property, bound doesn't
+    have it
+*/
+// function sayHi() {
+//     alert( this.name );
+// }
+
+// sayHi.test = 5;
+
+// let bound = sayHi.bind({
+//     name: "John"
+// });
+
+// alert( bound.test ); // what will be the output? why?
+
+
+// Fix a function that loses "this"
+// function askPassword(ok, fail) {
+//     let password = prompt("Password?", '');
+//     if (password == "rockstar") ok();
+//     else fail();
+// }
+
+// let user = {
+//     name: 'John',
+
+//     loginOk() {
+//         alert(`${this.name} logged in`);
+//     },
+
+//     loginFail() {
+//         alert(`${this.name} failed to log in`);
+//     },
+// };
+
+// // trying to pass in the loginOk/loginFail as methods.
+// // When called in askPassword they lose their "this", thus
+// // pre binding is needed on these functions
+// askPassword(user.loginOk.bind(user), user.loginFail.bind(user));
+
+function askPassword(ok, fail) {
+    let password = prompt("Password?", '');
+    if (password == "rockstar") ok();
+    else fail();
 }
-
-function delay(func, delay){
-
-    // Create a function,
-    // - use func.call to refer to
-    // x of function()
-    return function(x){
-        setTimeout(() => func.call(this, x), delay);
-    };
-
-}
-// create wrappers
-let f1000 = delay(f, 4000);
-let f1500 = delay(f, 8000);
-
-f1000("test"); // shows "test" after 1000ms
-f1500("test"); // shows "test" after 1500ms
-
-// - Debounce decorator / Throttle decorator
-function exampleFunction(something){
-    alert("I have awoken " + something + "!");
-}
-
-function debounce(func, delay){
-    let id;
-    
-    // Call func.apply(this, arguments)
-    // since we can access arguments regardless
-    // if the provided function has provided them 
-    // or not.
-    return function(){
-        clearTimeout(id);
-        // reset timer and start again
-        id = setTimeout(() => func.apply(this, arguments), delay);
-    };
-}
-
-function throttle(func, ms) {
-
-    // A determiner if method
-    // was throttled or not
-    let isThrottled = false,
-        savedArgs,
-        savedThis;
-
-    function wrapper() {
-
-        // If throttled, save recent arguments
-        // and this
-        if (isThrottled) { // (2)
-            savedArgs = arguments;
-            savedThis = this;
-            return;
-        }
-
-        // immediately call this if no throttle
-        func.apply(this, arguments); // (1)
-
-        // apply throttle timer
-        isThrottled = true;
-          setTimeout(function() {
-            isThrottled = false; // (3)
-            if (savedArgs) {
-                wrapper.apply(savedThis, savedArgs);
-                savedArgs = savedThis = null;
-                }
-        }, ms);
+  
+let user = {
+    name: 'John',
+  
+    login(result) {
+        alert( this.name + (result ? ' logged in' : ' failed to log in') );
     }
+};
 
-    return wrapper;
-}
-
-// let newFunc = debounce(exampleFunction, 1000);
-// setTimeout(newFunc("John"), 500);
-// setTimeout(newFunc("Josh"), 700);
+/*
+    Given that the login function requires a boolean argument means we also
+    have to bind it as well, thus we bind the user context with the corresponding
+    truth value.
+*/
+askPassword(user.login.bind(user, true), user.login.bind(user, false));
