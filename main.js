708cf46b63f980f3d1c0c8538e6cc5c1e114b796
megarage9000 <gtom_1984@hotmail.ca>
2020-07-12 23:27:40 -0700

    Accessor Properties
    - Link: https://javascript.info/property-accessors

--- main.js
+++ main.js
@@ -1,52 +1,27 @@
 // This is important
 /* Manual and Specifications: https://javascript.info/manuals-specifications*/
 
-// Property flags and descriptors
+// Accessor properties
 
 /*
     Summary:
+    
+        - Accessor properties: are functions that allow the user to set and get 
+        values from an object
 
-    - Object properties have several attributes other than value:
-        1. writable: if true, the value can be changed, else not
-        2. enumerable: if true, can be seen in for...in loops, else not
-        3. configurable: if true, can be deleted and modify other attributes,
-        else not
+        - denoted by:
 
-    - Descriptors are objects that store these attributes, you can access them
-    by using Object.getOwnPropertyDescriptor(obj, propertyName)
+            get : propName(){
 
-    - You can modify these descriptors by using Object.defineProperty(obj, propertyName,
-        descriptor)
+            }
 
-    - You can define mutliple using Object.defineProperties(obj{
-        prop1: descriptor,
-        prop2: descriptor,
-        ...
-    })
+            set : propName(){
 
-    - Other methods worth mentioning:
-
-        Property descriptors work at the level of individual properties.
-
-        There are also methods that limit access to the whole object:
-
-            Object.preventExtensions(obj)
-                Forbids the addition of new properties to the object.
-            Object.seal(obj)
-                Forbids adding/removing of properties. Sets configurable: f
-                alse for all existing properties.
-            Object.freeze(obj)
-                Forbids adding/removing/changing of properties. Sets configurable: 
-                false, writable: false for all existing properties.
-
-            And also there are tests for them:
-
-            Object.isExtensible(obj)
-                Returns false if adding properties is forbidden, otherwise true.
-            Object.isSealed(obj)
-                Returns true if adding/removing properties is forbidden, and all 
-                existing properties have configurable: false.
-            Object.isFrozen(obj)
-                Returns true if adding/removing/changing properties is forbidden, and 
-                all current properties are configurable: false, writable: false. 
+            }
+        
+        Descriptors:
+            - get: allows us to get a value from the object
+            - set: allows us to set a value on an object
+            - enumerable: same as data properties
+            - configurable: ""
 */
