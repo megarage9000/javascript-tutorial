3af2088b1236fad014ece82ee0632fe94deed79f
megarage9000 <gtom_1984@hotmail.ca>
2020-07-13 00:29:36 -0700

    F.Prototype
    - Link: https://javascript.info/function-prototype

--- main.js
+++ main.js
@@ -1,94 +1,46 @@
 // This is important
 /* Manual and Specifications: https://javascript.info/manuals-specifications*/
 
-// Prototypal Inheritence
+// F.Prototype
 
 /*
 
     Summary on website:
+    
+    In this chapter we briefly described the way of setting a [[Prototype]] for 
+    objects created via a constructor function. Later we’ll see more advanced 
+    programming patterns that rely on it.
 
-    
-    - In JavaScript, all objects have a hidden [[Prototype]] property that’s 
-    either another object or null.
-    
-    - We can use obj.__proto__ to access it (a historical getter/setter, there
-    are other ways, to be covered soon).
-    
-    - The object referenced by [[Prototype]] is called a “prototype”.
-    
-    - If we want to read a property of obj or call a method, and it doesn’t exist, 
-    then JavaScript tries to find it in the prototype.
-    
-    - Write/delete operations act directly on the object, they don’t use the prototype 
-    (assuming it’s a data property, not a setter).
-    
-    - If we call obj.method(), and the method is taken from the prototype, this still 
-    references obj. So methods always work with the current object even if they are inherited.
-    
-    - The for..in loop iterates over both its own and its inherited properties. 
-    All other key/value-getting methods only operate on the object itself.
+    Everything is quite simple, just a few notes to make things clear:
+
+        - The F.prototype property (don’t mistake it for [[Prototype]]) sets 
+        [[Prototype]] of new objects when new F() is called.
+        
+        - The value of F.prototype should be either an object or null: other 
+        values won’t work.
+
+        - The "prototype" property only has such a special effect when set on a
+        constructor function, and invoked with new.
+
+    On regular objects the prototype is nothing special:
 
 */
 
-// - Working with prototypes
-let animal = {
-    jumps: null
-};
-let rabbit = {
-    __proto__: animal,
-    jumps: true
-};
-  
-alert( rabbit.jumps ); // ? (1)
-// answer: true
+// See tasks on website for better understanding!
 
-delete rabbit.jumps;
+// example
+function User(name) {
+    this.name = name;
+  }
+User.prototype = {}; // (*)
 
-alert( rabbit.jumps ); // ? (2)
-// answer: null
+let user = new User('John');
+let user2 = new user.constructor('Pete');
 
-delete animal.jumps;
-
-alert( rabbit.jumps ); // ? (3)
-// answer: undefined
-
-
-// Searching algorithm
-
-let head = {
-    glasses: 1
-};
-
-let table = {
-    pen: 3,
-    __proto__: head
-};
-
-let bed = {
-    sheet: 1,
-    pillow: 2,
-    __proto__: table
-};
-
-let pockets = {
-    money: 2000,
-    __proto__: bed
-};
-
-alert(pockets.pen);
-alert(bed.glasses);
-
-// Where does it write?
-// - The rabbit recieves the 
-// this.full property
-let animal = {
-    eat() {
-        this.full = true;
-    }
-};
-
-let rabbit = {
-    __proto__: animal
-};
-  
-rabbit.eat();
+alert( user2.name ); // undefined
+/*
+  user2.name does the following:
+  - checks user.constructor, nothing there
+  - checks the prototype chain, only finding
+  empty javscript object in prototype :(
+*/
