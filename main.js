ce1110f24c2310b205864ba88b8e6acad2563acb
megarage9000 <gtom_1984@hotmail.ca>
2020-07-20 16:16:18 -0700

    Class Inhertitence
    - Link: https://javascript.info/class-inheritance

--- main.js
+++ main.js
@@ -1,117 +1,104 @@
 // This is important
 /* Manual and Specifications: https://javascript.info/manuals-specifications*/
 
-// Classes
+// Class Inheritence
 
 /**
- * The basic class syntax looks like this:
+ * Summary
+ *
+ *  - classes can inherit from other classes via extend
+ *      - syntax = class [child] extends [parent]
+ * 
+ *  - you can use super to overide methods of a class
+ *      - for constructors, super must be the first line of a class
+ *      if it inherits from another class     
+ *  
+ *  - class methods remember their object (their 'this') via internal [[HomeObject]] property
+ *      - used to help resolve plenty of 'super' issues
+ *      - for objects, the only way for methods to gain this property is by the
+ *      normal syntax func(), not func : function()
+ *      - not safe to copy method from one object to another if it contains super
+ **/
 
-    class MyClass {
-        prop = value; // property
-        // - Note, not in MyClass.prototype!
 
-        constructor(...) { // constructor
-            // ...
-        }
+ // Error: creating an instance
+ class Animal {
 
-        method(...) {} // method
-
-        get something(...) {} // getter method
-        set something(...) {} // setter method
-
-        [Symbol.iterator]() {} // method with computed name (symbol here)
-        // ...
-    }
-
-    MyClass is technically a function (the one that we provide as constructor), 
-    while methods, getters and setters are written to MyClass.prototype.
-
-    In the next chapters we’ll learn more about classes, including inheritance and other features.
- */
-
-// Rewrite Clock to class
-// function Clock({ template }) {
-  
-//     let timer;
-
-//     function render() {
-//         let date = new Date();
-
-//         let hours = date.getHours();
-//         if (hours < 10) hours = '0' + hours;
-
-//         let mins = date.getMinutes();
-//         if (mins < 10) mins = '0' + mins;
-
-//         let secs = date.getSeconds();
-//         if (secs < 10) secs = '0' + secs;
-
-//         let output = template
-//         .replace('h', hours)
-//         .replace('m', mins)
-//         .replace('s', secs);
-
-//         console.log(output);
-//     }
-
-//     this.stop = function() {
-//         clearInterval(timer);
-//     };
-
-//     this.start = function() {
-//         render();
-//         timer = setInterval(render, 1000);
-//     };
-  
-//   }
-
-class Clock {
-
-    constructor({template}){
-        this.template = template;
-    }
-
-    get template(){
-        return this._template;
-    }
-
-    set template(newTemplate){
-        this._template = newTemplate;
-    }
-
-    render() {
-        let date = new Date();
-
-        let hours = date.getHours();
-        if (hours < 10) hours = '0' + hours;
-
-        let mins = date.getMinutes();
-        if (mins < 10) mins = '0' + mins;
-
-        let secs = date.getSeconds();
-        if (secs < 10) secs = '0' + secs;
-
-        let output = this.template
-        .replace('h', hours)
-        .replace('m', mins)
-        .replace('s', secs);
-
-        console.log(output);
-    }
-    
-    stop(){
-        clearInterval(this.timer);
-    }
-
-    start(){
-        this.render();
-        this.timer = setInterval(() => this.render(), 1000);
+    constructor(name) {
+        this.name = name;
     }
 
 }
 
-let clock = new Clock({template: 'h:m:s'});
-clock.start();
+class Rabbit extends Animal {
+    constructor(name) {
+        super(name); // solution, call super() first
+        this.name = name;
+        this.created = Date.now();
+    }
+}
+  
+let rabbit = new Rabbit("White Rabbit"); // Error: this is not defined
+// alert(rabbit.name);
 
-// class version
+// Extended Clock
 
+class Clock {
+    constructor({ template }) {
+      this.template = template;
+    }
+  
+    render() {
+        let date = new Date();
+    
+        let hours = date.getHours();
+        if (hours < 10) hours = '0' + hours;
+    
+        let mins = date.getMinutes();
+        if (mins < 10) mins = '0' + mins;
+    
+        let secs = date.getSeconds();
+        if (secs < 10) secs = '0' + secs;
+    
+        let output = this.template
+            .replace('h', hours)
+            .replace('m', mins)
+            .replace('s', secs);
+    
+        console.log(output);
+    }
+  
+    stop() {
+        clearInterval(this.timer);
+    }
+  
+    start() {
+        this.render();
+        this.timer = setInterval(() => this.render(), 1000);
+    }
+}
+// solution from site, doesn't work though?
+class ExtendedClock extends Clock {
+    constructor(clockProperties){
+        super(clockProperties);
+        // Destructuring the passed in object
+        let {newPrecision = 1000} = clockProperties;
+        this.precision = newPrecision;
+    }
+
+    get precision(){
+        return this._precision;
+    }
+
+    set precision(newPrecision){
+        this._precision = newPrecision;
+    }
+
+    start() {
+        this.render();
+        this.timer = setInterval(() => this.render(), this.precision);
+    }
+};
+
+// clock = new ExtendedClock('h:m:s', 2000);
+// clock.start()
