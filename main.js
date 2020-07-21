b4a5376eea5c61b220e39f719d9004508cfccfd5
megarage9000 <gtom_1984@hotmail.ca>
2020-07-20 22:13:08 -0700

    Public, Private, Protected
    - Link: https://javascript.info/private-protected-properties-methods

--- main.js
+++ main.js
@@ -1,46 +1,19 @@
 // This is important
 /* Manual and Specifications: https://javascript.info/manuals-specifications*/
 
-// Static Methods / Static Properties
+// Public, Private, and Protected
 
 /**
  * Summary
  *
- * - Static methods are used for methods that belong to the class as a "whole",
- * not per instance of the class
+ * - Similar to Java, C# and C++; there are protected, public and [NEW FEATURE] private
+ * - protected fields are denoted as _[name]
+ * - private fields are dentoted as #[name] 
+ * - use getters and setters to describe its accessbility features and such
  * 
- * - creating a static method goes by the following:
- *  static [method name]() { } / (equivalent to [class].[methodName] = function(){})
- * 
- * - [Only for new Chrome browsers] creating a static field goes as follows:
- * static [fieldName] = value / (equivalent to [class].[fieldName] = value)
- * 
- * - static methods/fields are inherited
- * 
- * - useful for class wide operations, such as storage, comparisons, etc.
  **/
 
- // Class extends Object?
 
-// - needs super()
-/**
- *
- *  rabbit.__proto__ is different for both:
- *      - for extend Object:
- *      rabbit.__proto__ = Object
- *     - for just rabbit:
- *      rabbit.__proto__ = Function.prototype 
- **/
-class Rabbit extends Object {
-    constructor(name) {
-        // needs this
-        super();
-        this.name = name;
-    }
-}
 
-let rabbit = new Rabbit("Rab");
-
-alert( rabbit.hasOwnProperty('name') ); // Error
 
 
