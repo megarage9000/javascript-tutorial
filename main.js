4ca106e084c9a44b53598bc4f4ee7b8861d0f4a6
megarage9000 <gtom_1984@hotmail.ca>
2020-07-20 22:03:00 -0700

    Static methods / fields
    - Link: https://javascript.info/static-properties-methods

--- main.js
+++ main.js
@@ -1,104 +1,46 @@
 // This is important
 /* Manual and Specifications: https://javascript.info/manuals-specifications*/
 
-// Class Inheritence
+// Static Methods / Static Properties
 
 /**
  * Summary
  *
- *  - classes can inherit from other classes via extend
- *      - syntax = class [child] extends [parent]
+ * - Static methods are used for methods that belong to the class as a "whole",
+ * not per instance of the class
  * 
- *  - you can use super to overide methods of a class
- *      - for constructors, super must be the first line of a class
- *      if it inherits from another class     
- *  
- *  - class methods remember their object (their 'this') via internal [[HomeObject]] property
- *      - used to help resolve plenty of 'super' issues
- *      - for objects, the only way for methods to gain this property is by the
- *      normal syntax func(), not func : function()
- *      - not safe to copy method from one object to another if it contains super
+ * - creating a static method goes by the following:
+ *  static [method name]() { } / (equivalent to [class].[methodName] = function(){})
+ * 
+ * - [Only for new Chrome browsers] creating a static field goes as follows:
+ * static [fieldName] = value / (equivalent to [class].[fieldName] = value)
+ * 
+ * - static methods/fields are inherited
+ * 
+ * - useful for class wide operations, such as storage, comparisons, etc.
  **/
 
+ // Class extends Object?
 
- // Error: creating an instance
- class Animal {
-
+// - needs super()
+/**
+ *
+ *  rabbit.__proto__ is different for both:
+ *      - for extend Object:
+ *      rabbit.__proto__ = Object
+ *     - for just rabbit:
+ *      rabbit.__proto__ = Function.prototype 
+ **/
+class Rabbit extends Object {
     constructor(name) {
+        // needs this
+        super();
         this.name = name;
     }
-
 }
 
-class Rabbit extends Animal {
-    constructor(name) {
-        super(name); // solution, call super() first
-        this.name = name;
-        this.created = Date.now();
-    }
-}
-  
-let rabbit = new Rabbit("White Rabbit"); // Error: this is not defined
-// alert(rabbit.name);
+let rabbit = new Rabbit("Rab");
 
-// Extended Clock
+alert( rabbit.hasOwnProperty('name') ); // Error
 
-class Clock {
-    constructor({ template }) {
-      this.template = template;
-    }
-  
-    render() {
-        let date = new Date();
-    
-        let hours = date.getHours();
-        if (hours < 10) hours = '0' + hours;
-    
-        let mins = date.getMinutes();
-        if (mins < 10) mins = '0' + mins;
-    
-        let secs = date.getSeconds();
-        if (secs < 10) secs = '0' + secs;
-    
-        let output = this.template
-            .replace('h', hours)
-            .replace('m', mins)
-            .replace('s', secs);
-    
-        console.log(output);
-    }
-  
-    stop() {
-        clearInterval(this.timer);
-    }
-  
-    start() {
-        this.render();
-        this.timer = setInterval(() => this.render(), 1000);
-    }
-}
-// solution from site, doesn't work though?
-class ExtendedClock extends Clock {
-    constructor(clockProperties){
-        super(clockProperties);
-        // Destructuring the passed in object
-        let {newPrecision = 1000} = clockProperties;
-        this.precision = newPrecision;
-    }
 
-    get precision(){
-        return this._precision;
-    }
-
-    set precision(newPrecision){
-        this._precision = newPrecision;
-    }
-
-    start() {
-        this.render();
-        this.timer = setInterval(() => this.render(), this.precision);
-    }
-};
-
-// clock = new ExtendedClock('h:m:s', 2000);
-// clock.start()
